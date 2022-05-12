4fa059d3ecb2608a4837bdc69ba9d8007a73a5ad
erfanbashar <erfan.bashar.13@gmail.com>
2022-05-13 01:25:14 +0600

    completed initial draft and JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-function onloadBody () {
-    document.getElementById("mainDiv").style.backgroundColor = "lightblue";
+function toggleNavigation() {
+    let nav = document.getElementById("mobile-nav");
+    if (nav.classList.contains('w3-show')) {
+        nav.classList.remove('w3-show');
+    } else {
+        nav.classList.add('w3-show');
+    }
 }
