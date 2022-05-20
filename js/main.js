7e24036467e39769e13d0ec5ce46083c20b9d6ab
erfanbashar <erfan.bashar.13@gmail.com>
2022-05-20 10:57:14 +0600

    new branch

--- js/main.js
+++ js/main.js
@@ -6,3 +6,28 @@ function toggleNavigation() {
         nav.classList.add('w3-show');
     }
 }
+
+function getLinkedin() {
+    var linkedin = document.querySelector("#linkedin");
+    linkedin.href = "https://www.linkedin.com/in/erfanbashar/";
+}
+
+function getGithub() {
+    var github = document.querySelector("#github");
+    github.href = "https://github.com/erfanbashar";
+}
+
+function getFacebook() {
+    var facebook = document.querySelector("#facebook");
+    facebook.href = "https://www.facebook.com/erfan.bashar.7/";
+}
+
+function getInstagram() {
+    var instagram = document.querySelector("#instagram");
+    instagram.href = "https://www.instagram.com/erfanbashar13/";
+}
+
+function getCv() {
+    var cv = document.querySelector("#cv");
+    cv.href = "https://drive.google.com/file/d/1AsJhjHsWW67U3DXu2Vup-XMm_TEn5KFg/view?usp=sharing";
+}
