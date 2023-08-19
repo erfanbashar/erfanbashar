b59c95e25c7719d2ab14a6e62b81b5b8e2de5d81
Erfan Bashar <erfanbashar@users.noreply.github.com>
2023-08-20 00:16:06 +0600

    add country conditions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    let reveals = document.querySelectorAll(".reveal");
 
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+    for (let i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = reveals[i].getBoundingClientRect().top;
+        let elementVisible = 150;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
@@ -45,7 +45,7 @@ function loadAboutText() {
     aboutText.id = "aboutText";
     aboutText.style.textAlign = "justify";
     aboutText.className = "w3-margin-top-2";
-    aboutText.textContent = "A focused and quick-learning tech solutionist in the FinTech industry with more than 5 years of experience in development, customization, support, and manage enterprise applications. Provider of CBS application solution with expertise in working with Java web-based technologies, enterprise DB business logic configuration, and managing several technology projects in agile environments. Confident communicator and strategic thinker to build scalable softwares customizable to meet organizational needs and highlight core competencies.";
+    aboutText.textContent = "A focused, quick-learning tech solutionist in the FinTech industry with more than 6 years of experience in the development, customization, support, and manage enterprise applications. Expertise in CBS application solutions with expertise in working with Java web-based technologies and managing several technology projects in agile environments. Confident communicator and strategic thinker to build scalable software customizable to meet organizational needs";
 
     let aboutHeader = document.createElement("h2");
     aboutHeader.className = "w3-border-bottom w3-border-amber";
@@ -204,11 +204,59 @@ function loadEmploymentHistory() {
     mainDiv.appendChild(empHisHeader);
     let div = document.createElement("div");
 
+    getNagadExperience(div);
     getMtbExperience(div);
     getBblExperience(div);
+    
     mainDiv.appendChild(div);
 
     document.querySelector("#portfolio").appendChild(mainDiv);
+}
+
+function getNagadExperience(div) {
+    let organizationName = document.createElement("h4");
+    organizationName.innerHTML = '<span style="color: orange;">Digital Bank</span>' + ' | Nagad Limited';
+    div.appendChild(organizationName);
+
+    let designation = document.createElement("h4");
+    designation.innerHTML = '<span style="color: orange;">Lead Engineer, CBS Integration & Projects</span>' + ' | June 2023 - Present';
+
+    div.appendChild(designation);
+
+    let jobDescription = document.createElement("ul");
+    jobDescription.className = "w3-ul";
+    jobDescription.style.fontWeight = "500";
+    jobDescription.style.textAlign = "justify";
+    jobDescription.appendChild(createMenuItem("Designing overall solution architecture and implementation plan for CBS implementation."));
+    jobDescription.appendChild(createMenuItem("Identifying, tracking and reviewing CBS deliverables with efficient and quality code based on given specifications."));
+    jobDescription.appendChild(createMenuItem("Developing, fixing, and improving CBS functionalities according to Business / System requirement."));
+    jobDescription.appendChild(createMenuItem("Ensuring patch deployment planning and delivery of system integration."));
+    jobDescription.appendChild(createMenuItem("Providing technical support and expertise advice throughout all technical-functional CBS features."));
+    jobDescription.appendChild(createMenuItem("Identifying, defining and coordinating development activities for Change Request of CBS project."));
+    div.appendChild(jobDescription);
+}
+
+function getMtbExperience(div) {
+    let organizationName = document.createElement("h4");
+    organizationName.innerHTML = '<span style="color: orange;">Digital Banking Division</span>' + ' | Mutual Trust Bank Limited';
+    div.appendChild(organizationName);
+
+    let designation = document.createElement("h4");
+    designation.innerHTML = '<span style="color: orange;">Senior Officer</span>' + ' | July 2021 - June 2023';
+    div.appendChild(designation);
+
+    let jobDescription = document.createElement("ul");
+    jobDescription.className = "w3-ul";
+    jobDescription.style.fontWeight = "500";
+    jobDescription.style.textAlign = "justify";
+    jobDescription.appendChild(createMenuItem("Managed scope, budget, timeline, and end-to-end execution of technology projects"));
+    jobDescription.appendChild(createMenuItem("Coordinated and integrated CBS systems for business stakeholders to achieve efficient solutions."));
+    jobDescription.appendChild(createMenuItem("Constructed comprehensive project plans to bundle requirements with goals, and coordinated stakeholders during all project phases"));
+    jobDescription.appendChild(createMenuItem("Monitored project progress continuously and developed detailed reports on milestones and deliverables"));
+    jobDescription.appendChild(createMenuItem("Proactively identified problems, and proposed and implemented solutions to business stakeholders."));
+    jobDescription.appendChild(createMenuItem("Analyzed projects budget and provided feedback relating to cost-benefit and return-on-investment standards"));
+
+    div.appendChild(jobDescription);
 }
 
 function getBblExperience(div) {
@@ -216,7 +264,7 @@ function getBblExperience(div) {
     organizationName.innerHTML = '<span style="color: orange;">Technology Division</span>' + ' | BRAC Bank Limited';
     div.appendChild(organizationName);
 
-    var designation = document.createElement("h4");
+    let designation = document.createElement("h4");
     designation.innerHTML = '<span style="color: orange;">Senior Officer</span>' + ' | May 2019 - June 2021';
 
     div.appendChild(designation);
@@ -229,35 +277,12 @@ function getBblExperience(div) {
     jobDescription.className = "w3-ul";
     jobDescription.style.fontWeight = "500";
     jobDescription.style.textAlign = "justify";
-    jobDescription.appendChild(createMenuItem("Enhanced existing modules and develop new features to provide critical resolution, and analysed, modified, and supported for RCA and Permanent Resolution of the Core Banking Application."));
-    jobDescription.appendChild(createMenuItem("Analysed business requirements, produced system design to support upgrades and ongoing maintenance, defined problem-solving procedures through interaction with internal customers (up to L2) to prepare, compare and recommend alternate solutions."));
-    jobDescription.appendChild(createMenuItem("Wrote, tested, and debugged SQL/PL-SQL codes, stored procedures, views and functions, and other ETL processes to ensure optimal performance, reliability, and integrity."));
+    jobDescription.appendChild(createMenuItem("Enhanced existing modules and developed new features of the Core Banking Application - Finacle."));
+    jobDescription.appendChild(createMenuItem("Wrote, tested, and debugged codes, stored procedures, views, functions, ETL processes with SQL/PL-SQL"));
+    jobDescription.appendChild(createMenuItem("Configured API to integrate external applications with the Core Banking Application - Finacle"));
+    jobDescription.appendChild(createMenuItem("Analyzed business requirements, designed systems support upgrades and maintenance, and defined problem-solving procedures through interaction with internal customers (up to L2) towards solutions."));
     jobDescription.appendChild(createMenuItem("Supported business data analysis and developed customized reports for ad-hoc data requests."));
-    jobDescription.appendChild(createMenuItem("Worked in multiple projects with project managers, businesses owners, analyst teams, and clients for prototyped end-to-end analysis, implemented system validation, gathered BRS and SRS, UAT, and Post Production verification."));
-    jobDescription.appendChild(createMenuItem("Documented and reviewed code, provided reports, and peer feedback."));
-    div.appendChild(jobDescription);
-}
-
-function getMtbExperience(div) {
-    let organizationName = document.createElement("h4");
-    organizationName.innerHTML = '<span style="color: orange;">Digital Banking Division</span>' + ' | Mutual Trust Bank Limited';
-    div.appendChild(organizationName);
-
-    let designation = document.createElement("h4");
-    designation.innerHTML = '<span style="color: orange;">Senior Officer</span>' + ' | July 2021 - Present';
-    div.appendChild(designation);
-
-    let jobDescription = document.createElement("ul");
-    jobDescription.className = "w3-ul";
-    jobDescription.style.fontWeight = "500";
-    jobDescription.style.textAlign = "justify";
-    jobDescription.appendChild(createMenuItem("Managing projects throughout the entire execution period to ensure success in terms of scope, budget, and timeline."));
-    jobDescription.appendChild(createMenuItem("Working as the “missing middle” between business and CBS application to integrate systems for achieving efficient solutions."));
-    jobDescription.appendChild(createMenuItem("Developing and reviewing comprehensive project plans to bundle requirements with goals, and coordinating various stakeholders during all project phases, from initial development through implementation."));
-    jobDescription.appendChild(createMenuItem("Monitoring project progress continuously and making detailed scheduled reports on milestones and deliverables."));
-    jobDescription.appendChild(createMenuItem("Communicating proactively with all involved personnel to encourage, identify problems, create solutions, and implement efficiency improvements."));
-    jobDescription.appendChild(createMenuItem("Analysing economics of project plans and providing actionable feedback relating to cost-benefit and return-on-investment standards."));
-    jobDescription.appendChild(createMenuItem("Oversee acquisition of resources and materials as needed before and during projects by talking with customers, team members, managers, and in-house accountants and negotiating price and payment agreements with vendors."));
+    jobDescription.appendChild(createMenuItem("Collaborated with project managers and businesses owners for end-to-end analysis, implemented system validations, gathered BRS and SRS, UAT, Post-production verification, documented code, provided reports, performed code review, and peer feedback"));
     div.appendChild(jobDescription);
 }
 
@@ -277,28 +302,39 @@ function toggleNavigation() {
 }
 
 function getLinkedin() {
-    var linkedin = document.querySelector("#linkedin");
+    let linkedin = document.querySelector("#linkedin");
     linkedin.href = "https://www.linkedin.com/in/erfanbashar/";
 }
 
 function getGithub() {
-    var github = document.querySelector("#github");
+    let github = document.querySelector("#github");
     github.href = "https://github.com/erfanbashar";
 }
 
 function getFacebook() {
-    var facebook = document.querySelector("#facebook");
+    let facebook = document.querySelector("#facebook");
     facebook.href = "https://www.facebook.com/erfan.bashar.7/";
 }
 
 function getInstagram() {
-    var instagram = document.querySelector("#instagram");
+    let instagram = document.querySelector("#instagram");
     instagram.href = "https://www.instagram.com/erfanbashar13/";
 }
 
-function getCv() {
-    var cv = document.querySelector("#cv");
-    cv.href = "https://drive.google.com/file/d/12GfKbB0WbOGeLXNHz8Vu0uYfwUXXxkY8/view?usp=sharing";
+async function getCv() {
+    let cv = document.querySelector("#cv");
+
+    const request = await fetch("https://ipinfo.io/json?token=8d1dda29c0e932");
+    const jsonResponse = await request.json();
+    const orgCountry = jsonResponse.country;
+
+    if(orgCountry == 'BD') {
+        cv.href = "https://drive.google.com/file/d/12GfKbB0WbOGeLXNHz8Vu0uYfwUXXxkY8/view?usp=sharing";
+    }
+    else {
+        cv.href = "https://drive.google.com/file/d/11m5XjFKuj_wEieCRtb-nIrmabQ1zJHkm/view?usp=drive_link";
+    }
+    
 }
 
 // testimonial section
@@ -307,7 +343,7 @@ function loadTestimonial() {
     "use strict";
 
     if ($(".testimonials-carousel").length > 0) {
-        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+        let j2 = new Swiper(".testimonials-carousel .swiper-container", {
             preloadImages: false,
             slidesPerView: 1,
             spaceBetween: 20,
@@ -337,7 +373,7 @@ function loadTestimonial() {
 
 
     setInterval(function() {
-        var size = randomValue(sArray);
+        let size = randomValue(sArray);
         $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
         $('.individual-bubble').animate({
             'bottom': '100%',
